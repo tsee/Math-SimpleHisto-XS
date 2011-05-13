266b1962c5343d2d08cdf4eee36e7abf098ebc09
Steffen Mueller <smueller@cpan.org>
2011-05-13 18:13:10 +0200

    More documentation

--- lib/Math/SimpleHisto/XS.pm
+++ lib/Math/SimpleHisto/XS.pm
@@ -40,13 +40,59 @@ Math::SimpleHisto::XS - Simple histogramming, but kinda fast
   $hist->fill(\@xs);
   $hist->fill(\@xs, \@ws);
   
-  my $data_bins = $hist->all_bin_contents; # get bin contents
+  my $data_bins = $hist->all_bin_contents; # get bin contents as array ref
+  my $bin_centers = $hist->bin_centers; # dito for the bins
 
 =head1 DESCRIPTION
 
+This module implements simple 1D histograms with fixed bin size.
+The implementation is mostly in C with a thin Perl layer on top.
+
+If this module isn't powerful enough for your histogramming needs,
+have a look at the powerful-but-experimental L<SOOT> module or
+submit a patch.
+
+The lower bin boundary is considered part of the bin. The upper
+bin boundary is considered part of the next bin or overflow.
+
+=head1 METHODS
+
+=head2 new
+
+Constructor, takes named arguments. Mandatory parameters:
+
+=over 2
+
+=item min
+
+The lower boundary of the histogram.
+
+=item max
+
+The upper boundary of the histogram.
+
+=item nbins
+
+The number of bins in the histogram.
+
+=back
+
+=head2 fill
+
+Fill data into the histogram. Takes one or two arguments. The first must be the
+coordinate that determines where data is to be added to the histogram.
+The second is optional and can be a weight for the data to be added. It defaults
+to C<1>.
+
+If the coordinate is a reference to an array, it is assumed to contain many
+data points that are to be filled into the histogram. In this case, if the
+weight is used, it must also be a reference to an array of weights.
 
 =head1 SEE ALSO
 
+L<SOOT> is a dynamic wrapper around the ROOT C++ library
+which does histogramming and much more. Beware, it is experimental
+software.
 
 =head1 AUTHOR
 
