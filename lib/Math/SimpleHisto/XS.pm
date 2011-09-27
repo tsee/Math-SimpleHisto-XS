2ac6a310ffe34a7fb77c5099287400b6f0261bfe
Steffen Mueller <smueller@cpan.org>
2011-09-27 17:45:56 +0200

    version bump and changelog

--- lib/Math/SimpleHisto/XS.pm
+++ lib/Math/SimpleHisto/XS.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Carp qw(croak);
 
-our $VERSION = '1.13'; # Committed to floating point version numbers!
+our $VERSION = '1.20'; # Committed to floating point version numbers!
 
 require XSLoader;
 XSLoader::load('Math::SimpleHisto::XS', $VERSION);
